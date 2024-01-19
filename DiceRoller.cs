38074aa83cf8ada0aa87ed2b6840c8eb90adb247
c-woodman <cwoodma1@byu.edu>
2024-01-19 15:32:39 -0700

    split into two sheets

---
+++ DiceRoller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class DiceRoller
+    {
+        // Property to get the array storing roll totals
+        public int[] rollTotals { get; private set; }
+        // Constructor initializes the array for roll totals
+        public DiceRoller()
+        {
+            rollTotals = new int[13];
+        }
+
+        // Method to simulate rolling the dice a specified number of times
+        public int[] Rolling(int numRolls)
+        {
+
+            Random random = new Random();
+
+            //Loop for the number of times the user specified
+            for (int i = numRolls; i > 0; i--)
+            {
+
+                // Roll the first die
+                int die1 = random.Next(1, 7);
+                // Roll the second die
+                int die2 = random.Next(1, 7);
+
+                //Calculate the total of the two dice
+                int total = die1 + die2;
+
+                // Increment the count for the corresponding total in the array
+                rollTotals[total]++;
+
+
+
+            }
+            //Return the array with roll totals
+            return rollTotals;
+
+        }
+    }
+}
--- Mission2.cs
+++ Mission2.cs
@@ -2,87 +2,44 @@
 using System;
 using System.Xml.Linq;
 
-internal class Program
-{
-    private static void Main(string[] args)
+namespace Mission2 {
+    internal class Program
     {
-        //Create an instance of the DiceRoller class
-        DiceRoller dr;
-        dr = new DiceRoller();
-
-        // Prompt the user to enter the number of times to roll the dice
-        System.Console.WriteLine("How many times do you want to roll the dice?");
-        int numRolls = int.Parse(System.Console.ReadLine());
-
-        //Call the Rolling method of DiceRoller class, but the result is not used
-        dr.Rolling(numRolls);
-
-
-        // Call the Rolling method again and store the result in diceArray
-        int[] diceArray = dr.Rolling(numRolls);
-
-        // Loop through the array to print the distribution of dice rolls
-        for (int i = 2; i < diceArray.Length; i++)
+        private static void Main(string[] args)
         {
-            // Calculate the percentage for the current index
-            int percent = (diceArray[i] * 100) / numRolls;
+            //Create an instance of the DiceRoller class
+            DiceRoller dr = new DiceRoller();
 
-            // Print the index and a number of '*' characters based on the percentage
-            Console.Write($"{i}: ");
-            for (int j = 0; j < percent; j++)
+            // Prompt the user to enter the number of times to roll the dice
+            System.Console.WriteLine("How many times do you want to roll the dice?");
+            int numRolls = int.Parse(System.Console.ReadLine());
+
+            //Call the Rolling method of DiceRoller class, but the result is not used
+       // dr.Rolling(numRolls);
+
+
+            // Call the Rolling method again and store the result in diceArray
+            int[] diceArray = dr.Rolling(numRolls);
+
+            // Loop through the array to print the distribution of dice rolls
+            for (int i = 2; i < diceArray.Length; i++)
             {
-                Console.Write('*');
+                // Calculate the percentage for the current index
+                int percent = (diceArray[i] * 100) / numRolls;
+
+                // Print the index and a number of '*' characters based on the percentage
+                Console.Write($"{i}: ");
+                for (int j = 0; j < percent; j++)
+                {
+                    Console.Write('*');
+                }
+
+                // Move to the next line for the next index
+                Console.WriteLine();
+
             }
 
-            // Move to the next line for the next index
-            Console.WriteLine();
 
         }
-
-
-    }
-}
-
-
-internal class DiceRoller
-{
-    // Property to get the array storing roll totals
-    public int[] rollTotals
-    {
-        get; private set;
-    }
-    // Constructor initializes the array for roll totals
-    public DiceRoller()
-    {
-        rollTotals = new int[13];
-    }
-
-    // Method to simulate rolling the dice a specified number of times
-    public int[] Rolling(int numRolls)
-    {
-
-        Random random = new Random();
-
-        //Loop for the number of times the user specified
-        for (int i = numRolls; i > 0; i--)
-        {
-
-            // Roll the first die
-            int die1 = random.Next(1, 7);
-            // Roll the second die
-            int die2 = random.Next(1, 7);
-
-            //Calculate the total of the two dice
-            int total = die1 + die2;
-
-            // Increment the count for the corresponding total in the array
-            rollTotals[total]++;
-
-
-
-        }
-        //Return the array with roll totals
-        return rollTotals;
-
     }
 }
